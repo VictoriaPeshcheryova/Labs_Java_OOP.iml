3aa06110d8fa0dbc8a00c5d56a3efc947464f794
VictoriaPeshcheryova <77331720+VictoriaPeshcheryova@users.noreply.github.com>
2021-03-15 21:03:07 +0200

    sixth commit

---
+++ src/Lab4/BasketTest.java
@@ -0,0 +1,26 @@
+package Lab4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasketTest {
+
+    @Test
+    void TestAddToTheBasketTheProduct() {
+
+    }
+
+    @Test
+    void TestProductCheck(String nameOfProduct) {
+
+    }
+
+    @Test
+    void TestDeleteTheProductFromTheBasket() {
+    }
+
+    @Test
+    void TestPrintMyBasket() {
+    }
+}
---
+++ src/Lab4/CatalogTest.java
@@ -0,0 +1,24 @@
+package Lab4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CatalogTest {
+
+    @Test
+    void addTheProductToTheCatalog() {
+    }
+
+    @Test
+    void deleteTheProductFromTheCatalog() {
+    }
+
+    @Test
+    void printTheCatalog() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
