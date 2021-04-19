77e207242e2d543adb9cce90df8109abafe5d411
VictoriaPeshcheryova <77331720+VictoriaPeshcheryova@users.noreply.github.com>
2021-04-19 18:02:15 +0300

    15th commit

--- src/Lab5/main.java
+++ src/Lab5/main.java
@@ -12,12 +12,59 @@ import java.util.List;
 
 
 public class main {
+
     public static void main(String[] args) throws SQLException {
 
         UserDaoImpl udi=new UserDaoImpl();
         ProductDaoImpl pdi=new ProductDaoImpl();
+        /**Create table**/
+        udi.createUserTable();
+        pdi.createProductTable();
 
+        /**Insert a new record**/
+        User user = new User("admin","1111");
+        udi.insert(user);
 
+        Product product=new Product("Smart TV", "Apple",33000,true);
+        pdi.insert(product);
+
+        /**Select by id**/
+        User userSelect = udi.selectBYId(1);
+        System.out.println(userSelect.getId()+", "+userSelect.getLogin()+", "+userSelect.getPassword());
+
+        Product productSelect = pdi.selectBYId(1);
+        System.out.println(productSelect.getId()+", " +
+                ""+productSelect.getNameOfTheProduct()+", " +
+                ""+productSelect.getCompanyNameOfAProduct()+", " +
+                ""+productSelect.getPriceOfAProduct()+"," +
+                " "+ productSelect.isAvailable());
+
+        /**Delete person by id**/
+        udi.delete(2);
+        pdi.delete(2);
+
+        /**Update person**/
+        User userUpdate = new User("Tom","J3333");
+        udi.update(userUpdate,1);
+
+        Product productUpdate = new Product("Watches", "Samsung",3300,false);
+        pdi.update(productUpdate,1);
+
+        /**Select all persons**/
+
+        List<User> users = udi.selectAll();
+        for(User u : users) {
+            System.out.println(u.getId()+", "+u.getLogin()+", "+u.getPassword());
+        }
+
+        List<Product> products = pdi.selectAll();
+        for(Product p : products) {
+            System.out.println(p.getId()+", " +
+                    ""+p.getNameOfTheProduct()+", " +
+                    ""+p.getCompanyNameOfAProduct()+", " +
+                    ""+p.getPriceOfAProduct()+"," +
+                    " "+ p.isAvailable());
+        }
 
     }
 
